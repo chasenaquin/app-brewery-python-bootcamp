ef1cc06f958d363917eba8e2096f5c61c869c5d1
ronald.chase.naquin@gmail.com <chase.naquin@q6wjqx9rxr.lan>
2023-03-17 21:12:32 -0500

    Refactoring

--- 03_control_flow_and_logical_operators/leap_year_verification.py
+++ 03_control_flow_and_logical_operators/leap_year_verification.py
@@ -10,25 +10,27 @@ Type: Interactive Coding Exercise
 
 Description: Checking if an entered year is a leap year or not.
 
-Leap Year Logic
+Leap Year Logic:
 - On every year that is evenly divisible by 4
 - except every year that is evenly divisible by 100
 - unless the year is also evenly divisible by 400
 
+Associated File:
+leap_year_flow_chart.draw.io.xml
 """
 
 
 def determine_leapyear(year_):
     if year_ % 4 == 0:
-        return True
-    else:
-        if year_ % 100 == 0:
-            if year_ % 400 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
                 return True
             else:
                 return False
         else:
-            return False
+            return True
+    else:
+        return False
 
 
 year = int(input("What year would you like to check: "))
